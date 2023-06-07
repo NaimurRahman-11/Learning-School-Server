60bca02d6cc43f44399eeab6f60caae1b8ddbbac
Naimur Rahman <nirzon.naim@gmail.com>
2023-06-07 10:40:49 -0400

    User data saved

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
       //user related api
       app.post('/users', async (req, res) => {
           const user = req.body;
+
+          const query = { email: user.email }
+          const existingUser = await usersCollection.findOne(query);
+
+          if (existingUser) {
+              return res.send({message: 'User Already Exists'})
+          }
           const result = await usersCollection.insertOne(user);
           res.send(result);
       })

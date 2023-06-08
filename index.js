ead746d1df41127ba042bd7d5cd257234f469ef4
Naimur Rahman <nirzon.naim@gmail.com>
2023-06-08 03:36:04 -0400

    JWT implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,10 +10,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yg6iolk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,32 +45,78 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      
+    await client.connect();
 
 
-      const usersCollection = client.db('LearningDB').collection('users');
-      
 
-      //user related api
-      app.post('/users', async (req, res) => {
-          const user = req.body;
-
-          const query = { email: user.email }
-          const existingUser = await usersCollection.findOne(query);
-
-          if (existingUser) {
-              return res.send({message: 'User Already Exists'})
-          }
-          const result = await usersCollection.insertOne(user);
-          res.send(result);
-      })
+    const usersCollection = client.db('LearningDB').collection('users');
 
 
-      app.get('/users', async (req, res) => {
-          const result = await usersCollection.find().toArray();
-          res.send(result);
-      })
+
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token });
+    })
+
+
+    //user related api
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'User Already Exists' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({admin: false})
+      }
+
+
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
 
 
 
@@ -70,10 +134,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Learning is in progress')
+  res.send('Learning is in progress')
 })
 
 
 app.listen(port, () => {
-    console.log(`Learning on the port ${port}`);
+  console.log(`Learning on the port ${port}`);
 })
